e78518c7277d69d375262cfe11d14cad5a7e26eb
sivaramap <52095258+sivaramap@users.noreply.github.com>
2020-01-30 10:40:58 +0530

    Add files via upload

---
+++ docker_image.tf
@@ -0,0 +1,18 @@
+provider "docker" {
+    host = "ssh://psivarprasad@siva-instance-1:22"
+}
+resource "docker_image" "nginx" {
+  name = "nginx:1.11-alpine"
+}
+resource "docker_container" "nginx-server" {
+  name = "nginx-server"
+  image = "${docker_image.nginx.latest}"
+  ports {
+    internal = 80
+  }
+  volumes {
+    container_path  = "/usr/share/nginx/html"
+    host_path = "/home"
+    read_only = true
+  }
+}
